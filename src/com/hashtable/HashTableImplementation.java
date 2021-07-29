ff871987a6352a864562b36fe3693ec40c067f3a
Rohit Patro <rohitpatro6@gmail.com>
2021-07-29 20:18:25 +0530

    ADDED:Finding Frequency Of Words

--- src/com/hashtable/HashTableImplementation.java
+++ src/com/hashtable/HashTableImplementation.java
@@ -1,11 +1,32 @@
 package com.hashtable;
 
+import java.util.Map;
+import java.util.TreeMap;
 
 public class HashTableImplementation{
 	
-	public static void main(String[] args) {
-		System.out.println("Welcome To Hash Tables");
+	static void count (String str) {
+		Map<String,Integer> map = new TreeMap<>();
+
+		String arr[] = str.split(" ");
+		for (int i = 0;i<arr.length;i++) {
+			if (map.containsKey(arr[i])) {
+				map.put(arr[1],map.get(arr[i]) + 1);
+			}
+			else {
+				map.put(arr[i], 1);
+			}
+		}
+		for(Map.Entry<String,Integer> entry:
+			map.entrySet()) {
+			System.out.println(entry.getKey() + " -> "+entry.getValue());
+		}
 	}
-}
-	
+
+	public static void main(String[] args) {
+		String str = "To be or not to be";
+		count(str);
+	}
+	}
+
 	
